b5fa34f3200f400fc035ececd2846884391bcee7
Farik07 <38301192+Farik07@users.noreply.github.com>
2019-08-30 04:13:22 -0500

    Test 1

--- app/src/main/java/com/nightmaredevs/panicbutton/MainActivity.java
+++ app/src/main/java/com/nightmaredevs/panicbutton/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState); asasd
+        super.onCreate(savedInstanceState); asasdasda
         setContentView(R.layout.activity_main);
     }
 }
