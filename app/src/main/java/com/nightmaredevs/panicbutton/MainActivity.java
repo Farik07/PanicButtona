322f7348822fa481ebeed944c36602e9b5c8e157
Farik07 <38301192+Farik07@users.noreply.github.com>
2019-08-31 02:24:06 -0500

    Permisos agregados en el Manifest y solicitados la primera vez que la aplicación se ejecuta, si se quitan, se piden al iniciar de nuevo

--- app/src/main/java/com/nightmaredevs/panicbutton/MainActivity.java
+++ app/src/main/java/com/nightmaredevs/panicbutton/MainActivity.java
@@ -1,14 +1,27 @@
 package com.nightmaredevs.panicbutton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+
+    private void verifyPermission(){
+        String [] permissions = {Manifest.permission.ACCESS_FINE_LOCATION};
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),permissions[0])!= PackageManager.PERMISSION_GRANTED ) {
+
+            ActivityCompat.requestPermissions(MainActivity.this, permissions, 1);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        verifyPermission();
     }
 }
